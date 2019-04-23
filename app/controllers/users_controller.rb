99d31d106ad158e056f3bb01dfc5500deb2d0f35
Paul Lu <luhanfeng1106@sina.com>
2019-04-23 18:18:19 +0800

    add token structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def login; end
+  def login
+    if params[:code].nil?
+      # retreive the user info if they already have one
+      token = params[:token]
+    else
+      # send info to the wehat api to get open id and store them into the storage
+      RestClient.get("https://api.weixin.qq.com/sns/jscode2session?appid=#{APPID}&secret=#{SECRET_KEY}&js_code=sdasds&grant_type=authorization_code")
+    end
+  end
 end
