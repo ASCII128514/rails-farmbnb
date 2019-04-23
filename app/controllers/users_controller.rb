62f25bab9bd70ae480ed4e336ddd971a8a591a67
Paul Lu <luhanfeng1106@sina.com>
2019-04-23 19:25:11 +0800

    add authen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,12 @@ class UsersController < ApplicationController
       token = params[:token]
     else
       # send info to the wehat api to get open id and store them into the storage
-      RestClient.get("https://api.weixin.qq.com/sns/jscode2session?appid=#{APPID}&secret=#{SECRET_KEY}&js_code=sdasds&grant_type=authorization_code")
+      token = RestClient.get("https://api.weixin.qq.com/sns/jscode2session?appid=#{APPID}&secret=#{SECRET_KEY}&js_code=sdasds&grant_type=authorization_code")
+      payload = { token: token }
+      authen = JWT.encode payload, nil, 'none'
+      render json: {
+        authen: authen
+      }
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,42 +15,23 @@ ActiveRecord::Schema.define(version: 2019_04_23_055704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "inventories", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_inventories_on_user_id"
-  end
-
-  create_table "inventory_items", force: :cascade do |t|
-    t.bigint "inventory_id"
-    t.integer "quantity"
-    t.bigint "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["inventory_id"], name: "index_inventory_items_on_inventory_id"
-    t.index ["product_id"], name: "index_inventory_items_on_product_id"
-  end
-
   create_table "listings", force: :cascade do |t|
-    t.bigint "user_id"
     t.bigint "product_id"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_listings_on_product_id"
-    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
     t.bigint "product_id"
-    t.bigint "order_id"
+    t.bigint "shopping_cart_id"
     t.integer "quantity"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_orders_on_order_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["shopping_cart_id"], name: "index_orders_on_shopping_cart_id"
   end
 
   create_table "price_histories", force: :cascade do |t|
@@ -66,8 +47,11 @@ ActiveRecord::Schema.define(version: 2019_04_23_055704) do
     t.float "product_price"
     t.text "description"
     t.string "picture_url"
+    t.string "unit"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "shopping_carts", force: :cascade do |t|
@@ -79,7 +63,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_055704) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean "is_farmer"
+    t.string "name"
     t.text "description"
     t.string "profile_picture_url"
     t.string "openId"
@@ -87,13 +71,10 @@ ActiveRecord::Schema.define(version: 2019_04_23_055704) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "inventories", "users"
-  add_foreign_key "inventory_items", "inventories"
-  add_foreign_key "inventory_items", "products"
   add_foreign_key "listings", "products"
-  add_foreign_key "listings", "users"
-  add_foreign_key "orders", "orders"
   add_foreign_key "orders", "products"
+  add_foreign_key "orders", "shopping_carts"
   add_foreign_key "price_histories", "products"
+  add_foreign_key "products", "users"
   add_foreign_key "shopping_carts", "users"
 end
