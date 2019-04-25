34c43acaa0e5fb05660c6fc7f3fbf49b907ce7e2
Paul Lu <luhanfeng1106@sina.com>
2019-04-25 14:23:06 +0800

    fix

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -47,7 +47,7 @@ class Api::V1::ProductsController < Api::V1::BaseController
 
   def index
     @products = Product.all
-    render json: { user: @products}
+    render json: { user: @products }
   end
 
   private
